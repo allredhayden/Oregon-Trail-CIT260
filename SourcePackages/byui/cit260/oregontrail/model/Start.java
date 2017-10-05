5fe2830ea06324a314bb68a619b905f97094e21b
James <alkireson@gmail.com>
2017-10-05 07:28:15 -0500

    Modified "start" class to include empty main method.

--- SourcePackages/byui/cit260/oregontrail/model/Start.java
+++ SourcePackages/byui/cit260/oregontrail/model/Start.java
@@ -3,9 +3,8 @@ package byui.cit260.oregontrail.model;
 public class Start {
 
 	public static void main(String[] args) {
-		System.out.println("This is a placeholder.");
+	// Not yet constructed.
 	}
-
 }
 
 
