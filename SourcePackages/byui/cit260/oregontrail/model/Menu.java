7105a5ccaea886d2221305e558da978d05e8f3e8
James <alkireson@gmail.com>
2017-10-08 01:31:28 -0500

    Added hashCode & equals methods to Main class.

--- SourcePackages/byui/cit260/oregontrail/model/Menu.java
+++ SourcePackages/byui/cit260/oregontrail/model/Menu.java
@@ -35,10 +35,34 @@ public class Menu implements Serializable {
     }
 
     @Override
+    public int hashCode()
+    {
+    	final int prime = 31;
+    	int result = 1;
+    	result = prime * result + (menuStatus ? 1231 : 1237);
+    	result = prime * result + (options ? 1231 : 1237);
+    	return result;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+    	if (this == obj)
+    		return true;
+    	if (obj == null)
+    		return false;
+    	if (getClass() != obj.getClass())
+    		return false;
+    	Menu other = (Menu) obj;
+    	if (menuStatus != other.menuStatus)
+    		return false;
+    	if (options != other.options)
+    		return false;
+    	return true;
+    }
+
+    @Override
     public String toString() {
         return "Menu{" + "menuStatus=" + menuStatus + ", options=" + options + '}';
     }
-     
-    
-   
+
 }
