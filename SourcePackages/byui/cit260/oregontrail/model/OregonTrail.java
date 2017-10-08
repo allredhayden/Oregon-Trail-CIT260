85ab94283bc2fdb43041da2a533a2996a54ecdc6
Hayden Allred <haydenallred@gmail.com>
2017-10-08 17:38:42 -0600

    Added code to test my new classes for the individual assignment.

--- SourcePackages/byui/cit260/oregontrail/model/OregonTrail.java
+++ SourcePackages/byui/cit260/oregontrail/model/OregonTrail.java
@@ -14,7 +14,31 @@ public class OregonTrail {
 		double playerOneTime = playerOne.getBestTime();
 		
    	    System.out.println(playerOne.toString());
-		System.out.println(Actor.Banker.getName());
+            System.out.println(Actor.Banker.getName());
+            
+            //test Location.java
+            Location location = new Location();
+            location.setAmountRemaining(1);
+            location.setColumn(2);
+            location.setCurrentGeography("Idaho");
+            location.setRow(3);
+            location.setVisited(true);
+            System.out.println(location.toString());
+            
+            //test Score.java
+            Score score = new Score();
+            score.setCurrentScore(1000);
+            score.setHighScore(1588);
+            score.setRequiredScore(1250);
+            int[] values = {3, 6};
+            score.setScoreList(values);
+            System.out.println(score.toString());
+            
+            //test Weather.java
+            Weather weather = new Weather();
+            weather.setWeatherType("Windy");
+            System.out.println(weather.toString());
+            
 	}
 }
 
