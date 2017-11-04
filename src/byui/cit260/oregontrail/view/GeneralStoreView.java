a1f5b72ee060da6ce85575a66525374dc3f25a89
furnbla <fur14002@byui.edu>
2017-11-04 13:41:15 -0600

    Just working on the control layer

--- src/byui/cit260/oregontrail/view/GeneralStoreView.java
+++ src/byui/cit260/oregontrail/view/GeneralStoreView.java
@@ -11,7 +11,10 @@ import java.util.Scanner;
 
 public class GeneralStoreView {
     
-    private String menu =
+    // Need to use getMoney() later.
+    public static double money = 2000;
+    
+    private static String menu =
     
 "\n**** The Oregon Trail - Matt's General Store****"
 +"\n     Choose an item to add to you order:       *"
@@ -20,31 +23,31 @@ public class GeneralStoreView {
 +"\n     3. Clothing                       $0      *"
 +"\n     4. Ammunition                     $0      *"
 +"\n     5. Spare Parts                    $0      *"
-+"\n            Total Bill                 $0      *"
-+"\n            Total Funds                $800    *"
-+"\n            Funds Remaining            $800    *"
++"\n     6. Quit                                   *"
++"\n            Total Funds                $" + money + " *"
 +"\n What is you choice? (space bar to continue)   *"
 +"\n***********************************************";
     
    public GeneralStoreView() {
-        System.out.println(menu);
+        System.out.println(menu); // Displays menu / choices
         displayGeneralStoreView();
     }
    
-   public void displayGeneralStoreView() {
+   public static void displayGeneralStoreView() {
         boolean done = false;
         do {
-            // prompt for and get players name
-            String AmountOption = this.getAmountOption();
-            if (AmountOption.toUpperCase().equals("Q"))
+            // Call getAmountOption, assign returned value to AmountOption variable
+            String amountOption = getAmountOption();
+            if (amountOption.toUpperCase().equals("Q"))
                 return;
             
-            // do the requested action and display the next view
-            done = this.doAction(AmountOption);
+            // Call doAction, pass amountOption into parameters
+            done = doAction(amountOption);
         } while (!done);
     }
     
-    private String getAmountOption() {
+   // Read input, tell user if input is invalid
+    private static String getAmountOption() {
         
         Scanner keyboard = new Scanner(System.in);
         String value = "";
@@ -67,17 +70,78 @@ public class GeneralStoreView {
         return value;
     }
     
-    private boolean doAction(String choice) {
+    // Handle various input choices.
+    private static boolean doAction(String choice) {
+        double price = 0;
         choice = choice.toUpperCase();
         
         switch (choice) {
-        case "1 - 800": // create and start a new game
-            this.selectAmount();
+        case "1":
+            // Request item price, assign to 'price' variable. Tell user item price.
+            price = getItemPrice("oxen");
+            System.out.println("The cost of your item is " + price + ".");
+
+            // Subtract price from money. Tell user total money left after purchase.
+            money = money - price;
+            System.out.println("You have " + money + " dollars left.");           
             break;
+        case "2":
+            price = getItemPrice("food");
+            System.out.println("The cost of your item is " + price + ".");
+
+            money = money - price;
+            System.out.println("You have " + money + " dollars left.");
+            break;            
+        case "3":
+            price = getItemPrice("clothing");
+            System.out.println("The cost of your item is " + price + ".");
+
+            money = money - price;
+            System.out.println("You have " + money + " dollars left.");
+            break;            
+        case "4":
+            price = getItemPrice("ammunition");
+            System.out.println("The cost of your item is " + price + ".");      
+
+            money = money - price;
+            System.out.println("You have " + money + " dollars left.");
+            break;
+        case "5":
+            price = getItemPrice("spareparts");
+            System.out.println("The cost of your item is " + price + ".");      
+
+            money = money - price;
+            System.out.println("You have " + money + " dollars left.");
+            break;
+        case "6": // Quit, i.e. go to main menu.
+            MainMenuView mainMenuView = new MainMenuView();
         default:
             System.out.println("\n*** Invalid selection *** Try again");
+            break;            
         }
         return false;
+    }
+    
+    // Returns item price of specified item to caller.
+    private static double getItemPrice(String choice) {
+        double itemCost = 0;
+        
+        if (choice == "oxen") {
+            itemCost = 200;
+        }
+        if (choice == "food") {
+            itemCost = 50;
+        }
+        if (choice == "clothing") {
+            itemCost = 15;
+        }
+        if (choice == "ammunition") {
+            itemCost = 100;
+        }
+        if (choice == "spareparts") {
+            itemCost = 80;
+        }
+        return itemCost;
     }
     
     
