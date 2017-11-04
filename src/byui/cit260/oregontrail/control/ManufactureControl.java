2ed271936669001e72cca5fc2f0d8addf2789785
Hayden Allred <haydenallred@gmail.com>
2017-11-04 16:04:29 -0600

    no message

--- src/byui/cit260/oregontrail/control/ManufactureControl.java
+++ src/byui/cit260/oregontrail/control/ManufactureControl.java
@@ -13,10 +13,8 @@ public class ManufactureControl implements Serializable {
     double amount;
     double noPeople;
     boolean inventoryItem;
-    
-    }
 
-public double manufactureItem (double requiredResources,double item, double amount, double noPeople){
+    public double manufactureItem(double requiredResources,double item, double amount, double noPeople) {
            this.requiredResources = requiredResources;
            this.item = item;
            this.amount = amount;
@@ -25,24 +23,25 @@ public double manufactureItem (double requiredResources,double item, double amou
            
         if (requiredResources < 0 || requiredResources > 1){
                 System.out.println ("The value of requiredResources must be 1, cheater.");
-                 return manufactureItem;
-}
+                 return 0;
+        }
         if (noPeople < 0){
                 System.out.println ("You cannot have a negative number for people.");
-                 return manufactureItem;
-}
+                 return 0;
+        }
           if (item < 0){
                 System.out.println ("You cannot have a negative number for items."); 
-                 return manufactureItem;
+                 return 0;
           
-}
+        }
 
         if (amount > 100){
-                System.out.println ("You can't have more than 100 of an amount")                 
-                 return manufactureItem;
-}
+                System.out.println ("You can't have more than 100 of an amount");                 
+                 return 0;
+        }
         else{
-                 return manufactureItem;
-}
+                 return 0;
+        }
 
+    }
 }
