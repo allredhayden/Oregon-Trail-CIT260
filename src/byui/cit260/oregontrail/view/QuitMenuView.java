617204c0b469346a565f9df4efe5b9648a41818e
furnbla <fur14002@byui.edu>
2017-11-11 16:48:18 -0700

    My try at another view layer for Week 9.

---
+++ src/byui/cit260/oregontrail/view/QuitMenuView.java
@@ -0,0 +1,38 @@
+package byui.cit260.oregontrail.view;
+
+import byui.cit260.oregontrail.control.*;
+import byui.cit260.oregontrail.model.*;
+import java.util.Scanner;
+
+
+public class QuitMenuView extends View {
+    
+    public QuitMenuView() {
+        super(
+         "\n************** The Oregon Trail *******"
+        +"\n     Are you sure you want to quit?   *"
+        +"\n     1. Yes                           *"
+        +"\n     2. No                            *"
+        +"\n     What is your choice?             *"
+        +"\n***************************************");
+        super.display();
+    }
+
+    @Override
+    public boolean doAction(String choice)
+    {
+        choice = choice.toUpperCase();
+
+        switch (choice)
+        {
+        case "1":
+            System.out.print("\nProgram is terminated!\n");
+            break;
+        case "2":
+            MainMenuView mainMenuView = new MainMenuView();
+        default:
+            System.out.println("\n*** Invalid selection *** Try again");
+        }
+        return false;
+    }
+}
