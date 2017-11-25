b67bac2e66f9be478b534993bfd835fe027998d3
Hayden Allred <haydenallred@gmail.com>
2017-11-24 22:02:39 -0700

    Added ManufactureView, ManufactureControlException, and edited ManufactureControl.

--- src/byui/cit260/oregontrail/control/ManufactureControl.java
+++ src/byui/cit260/oregontrail/control/ManufactureControl.java
@@ -7,26 +7,46 @@ package byui.cit260.oregontrail.control;
 
 import java.io.Serializable;
 
+import byui.cit260.oregontrail.exceptions.ManufactureControlException;
+
 public class ManufactureControl implements Serializable {
+    
+    boolean valid = false;
+
+    public ManufactureControl() {
+    }
     
     // We will use getStrikesLeft() in the future.
     
-    public int acquireResources(int strikesLeft, int resourceAmount) {
+    public int acquireResources(int strikesLeft, int strength) throws ManufactureControlException {
         
-        int barrelsHarvested = (strikesLeft * resourceAmount);
+        int barrelsHarvested = (strikesLeft * strength);
         
-        if (resourceAmount < 0) {
-            System.out.println("You cannot have a negative number for amount.\n");
+        if (strength < 0) {
+            valid = false;
+            throw new ManufactureControlException("\nYou cannot have a negative number for strength.\n");
         }
         else if (strikesLeft < 0) {
-            System.out.println("Strikes can't be negative.\n");
+            valid = false;
+            throw new ManufactureControlException("\nStrikes can't be negative.\n");
         }
-        else if (resourceAmount > 1000) {
-            System.out.println("Amount can't be above 1000.\n");
+        else if (strength > 1000) {
+            valid = false;
+            throw new ManufactureControlException("\nStrength can't be above 1000.\n");
         }
         else {
-            System.out.println("Congratulations! You have harvested " + barrelsHarvested + " barrels of berries.\n");
+            valid = true;
+            System.out.println("\nCongratulations! You have harvested " + barrelsHarvested + " barrels of berries.\n");
         }
+        
         return barrelsHarvested;
     }
+    
+    public boolean isValid() {
+        return valid;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
+    }    
 }
